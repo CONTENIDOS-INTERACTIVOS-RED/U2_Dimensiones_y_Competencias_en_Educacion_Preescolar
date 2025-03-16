f6da50b818d46c61e49f55db6b24eefd9970f19a
AndresDiaz <devcontenidosinteractivos@gmail.com>
2025-03-16 17:58:18 -0500

    tema1 OK
    Se finaliza tema 1

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -1,10 +1,256 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(:subTitulo="'1. Título tema'")
+  BannerInterno(:subTitulo="'1. Dimensiones del desarrollo en la educación infantil'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
+    .bg-color-1.mb-5(data-aos="fade-up")
+      .row.justify-content-center.align-items-center
+        .col-lg-auto.mb-3.mb-lg-0
+          figure
+            img(src='@/assets/curso/tema1/1.png', alt='')
+        .col-lg
+          .p-4
+            p.mb-0(data-aos="fade-up") El desarrollo infantil es un proceso complejo donde cada experiencia, interacción y aprendizaje contribuye a la construcción del ser. Comprender las dimensiones del desarrollo en la educación infantil permite identificar su impacto en el crecimiento integral de los niños y niñas, así como las estrategias para potenciarlo en el aula. A continuación, se presentan los pilares fundamentales del desarrollo infantil y su influencia en la educación.
+    
+
     #t_1_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 1.1] Subtítulo tema
+      h2 #[span 1.1] Dimensión socioafectiva
+    
+    p.mb-4 El desarrollo socioafectivo es clave para el bienestar y el éxito académico y social, ya que ayuda a los niños a desarrollar habilidades como la empatía, la comunicación y la resolución de conflictos (Bronfenbrenner, 1987). El entorno juega un papel crucial: un ambiente afectuoso favorece la confianza y la autoestima, mientras que la falta de apoyo emocional puede generar dificultades sociales (Ainsworth, 1979).
+
+    .bg-full-width-2.bg-fondo-1
+      .px-4.px-md-5.pb-md-3
+
+        p En el aula, se pueden aplicar estrategias como:
+    
+        .row.justify-content-center.align-items-stretch.mb-5
+          .col-lg-4.mb-4(data-aos="zoom-in-up")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/2.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Juego en grupo
+                p.mb-0 Facilita la socialización y la cooperación.
+          .col-lg-4.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/3.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Expresión artística
+                p.mb-0 Ayuda a gestionar emociones de forma creativa.
+          .col-lg-4.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/4.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Modelo docente
+                p.mb-0 El comportamiento del educador influye en la formación de relaciones basadas en el respeto y la confianza (bisquerra, 2000).
+    
+    #t_1_2.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.2] Dimensión corporal
+    
+    p.mb-4 El desarrollo corporal es esencial para la exploración, la autonomía y el bienestar. Incluye aspectos como la motricidad gruesa y fina, la coordinación y el desarrollo físico (Gallahue & Donnelly, 2008). Estos aspectos están vinculados con habilidades futuras como la escritura (Pellegrini & Smith, 1998). Para fortalecer esta dimensión en el aula, se recomienda:
+
+    .row.justify-content-center.align-items-stretch.mb-3
+      .col-lg-6
+        .bg-color-2.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/5.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5 Actividades lúdicas
+              p.mb-0 Juegos de movimiento, circuitos motores y deportes adaptados.
+
+      .col-lg-6
+        .bg-color-2.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/6.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5 Espacios seguros
+              p.mb-0 Fomentar el juego libre en un entorno que permita la exploración.
+    .row.justify-content-center.align-items-stretch.mb-4
+      .col-lg-6
+        .bg-color-2.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/7.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5 Lenguaje corporal
+              p.mb-0 Su uso como herramienta educativa favorece la expresión y el aprendizaje.
+
+      .col-lg-6
+        .bg-color-2.p-4(data-aos="fade-left").h-100
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/8.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              h5 Rol del docente
+              p.mb-0 Generar un ambiente donde el movimiento sea valorado como una estrategia para mejorar la concentración y el desarrollo cognitivo.
+
+    #t_1_3.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.3] Dimensión comunicativa
+    
+    p.mb-4 Desde el nacimiento, los niños utilizan formas de comunicación como el llanto, los gestos y las expresiones faciales para interactuar. La comunicación abarca el lenguaje oral, gestual y escrito, influenciado por la calidad de las interacciones sociales (Vygotsky, 1978). Para estimular esta dimensión, es clave:
+
+    .bg-full-width.bg-h85
+      .px-4.px-md-5.pb-md-3
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            .row.justify-content-center.align-items-center
+              .col-lg-8(data-aos="fade-left").mb-3.mb-lg-0.mt-0.mt-lg-5
+                ul.lista-ul--color
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Narración de cuentos y juego simbólico:</b> fomentan la expresión oral y el pensamiento crítico.
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Ambientes ricos en estímulos lingüísticos:</b> aseguran el desarrollo del lenguaje (Bruner, 1983).
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Interacciones sociales:</b> favorecen la construcción del conocimiento y la adquisición del lenguaje.
+              .col-lg-4(data-aos="zoom-in")
+                img.img-fluid(src='@/assets/curso/tema1/9.svg', style="max-width:486px", alt="" ).mx-auto.mx-lg-0.ms-lg-auto.mb-4.mb-lg-0
+    
+    .bg-full-width.bg-color-5.mb-5
+      .px-4.px-md-5.py-4
+        p.mb-0 El lenguaje es más que una herramienta de comunicación: es fundamental para la construcción del pensamiento y la identidad. ¿Cómo pueden los docentes potenciar la comunicación en los niños dentro y fuera del aula?
+    
+    #t_1_4.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.4] Dimensión cognitiva
+    
+    p.mb-4 El desarrollo cognitivo permite a los niños percibir, interpretar y resolver problemas. Jean Piaget propuso cuatro etapas para explicar este proceso:
+
+    .bg-full-width.bg-color-info
+      .p-4.p-md-5
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-8
+            ImagenInfografica.color-secundario
+              template(v-slot:imagen)
+                figure
+                  img(src='@/assets/curso/tema1/10.svg', alt='', style="max-width: 1106px;").mx-auto
+    
+              .bg-color-white.box-shadow.p-3(x="24%" y="67%" numero="+")
+                h5 Sensorio motora (0-2 años)
+                p Aprendizaje a través de los sentidos y el movimiento.
+    
+              .bg-color-white.box-shadow.p-3(x="38.5%" y="46.5%" numero="+")
+                h5 Pre operacional (2-7 años)
+                p Desarrollo del pensamiento simbólico, aunque egocéntrico.
+    
+              .bg-color-white.box-shadow.p-3(x="56%" y="27%" numero="+")
+                h5 Operaciones concretas (7-11 años)
+                p Aplicación del razonamiento lógico en situaciones concretas.
+    
+              .bg-color-white.box-shadow.p-3(x="81%" y="6.5%" numero="+")
+                h5 Operaciones formales (11 años en adelante)
+                p Capacidad de pensamiento abstracto y razonamiento complejo.
+    
+    #t_1_5.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.5] Dimensión estética
+    
+    p.mb-4 El arte y la belleza son esenciales en la educación infantil, ya que favorecen la creatividad, la imaginación y la sensibilidad (Gardner, 1983). La dimensión estética permite a los niños desarrollar habilidades motoras, procesar la realidad de manera simbólica y reforzar su autoestima. Estrategias para fomentarla incluyen:
+
+    .bg-full-width.bg-color-2.mb-5
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-auto
+            img(src="@/assets/curso/tema1/11.svg", style="max-width: 90px").mx-auto
+          .col-lg
+            p.mb-0 El desarrollo cognitivo se ve influenciado por la interacción social (Vygotsky, 1978), la experiencia y la estimulación temprana. Estrategias como el aprendizaje basado en la indagación, juegos de resolución de problemas y trabajo colaborativo fomentan el pensamiento crítico, lógico y creativo.
+    
+    .bg-full-width-2.bg-fondo-2
+      .px-4.px-md-5.pb-md-3
+        div.row.justify-content-center.align-items-stretch.mb-5
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema1/12.svg" alt="" style="width: 90px")
+              h5.text-center Exploración sensorial
+              p.mb-0.text-center  Uso de materiales con diferentes texturas y colores.
+        
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema1/13.svg" alt="" style="width: 90px")
+              h5.text-center Arte interdisciplinario
+              p.mb-0.text-center  Integración del arte con otras áreas del conocimiento.
+        
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema1/14.svg" alt="" style="width: 90px")
+              h5.text-center Ambientes inspiradores
+              p.mb-0.text-center  Creación de espacios atractivos con obras de arte.
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema1/15.svg" alt="" style="width: 90px")
+              h5.text-center Experiencias culturales
+              p.mb-0.text-center  Exposición a diversas manifestaciones artísticas.
+
+        p.mb-4 El arte no es un lujo, sino una herramienta esencial para la formación integral de los niños. ¿Cómo puede fomentarse la sensibilidad artística dentro y fuera del aula?
+
+        #t_1_6.titulo-segundo(data-aos="flip-up")
+          h2 #[span 1.6] Dimensión espiritual
+    
+    p.mb-4 La espiritualidad no se limita a creencias religiosas, sino que involucra la búsqueda de significado, la conexión con uno mismo y con el entorno. Favorece el desarrollo emocional, el sentido de propósito y la resiliencia. Para estimular esta dimensión en la educación infantil, se pueden aplicar estrategias como:
+
+    .bg-full-width.bg-color-2
+      .px-4.px-md-5.pb-md-3
+        .row.justify-content-center.align-items-center
+          .col-lg-12
+            .row.justify-content-center.align-items-center
+              .col-lg-8(data-aos="fade-left").order-1.order-lg-2.mb-3.mb-lg-0.mt-0.mt-lg-5
+                ul.lista-ul--color
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Cultivar la introspección:</b> espacios de reflexión para expresar emociones y pensamientos.
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Fomentar la empatía:</b> mediante historias y experiencias colaborativas.
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Conexión con la naturaleza:</b> exploración del entorno natural para reforzar el respeto por la vida.
+                  li.d-flex
+                    i.far.fa-arrow-alt-circle-right.bg-color-4
+                    p.mb-0 <b>Estimular el pensamiento reflexivo:</b> preguntas abiertas sobre la existencia y la interconexión de los seres humanos.
+              .col-lg-4(data-aos="zoom-in").order-2.order-lg-1
+                img.img-fluid(src='@/assets/curso/tema1/16.svg', style="max-width:486px", alt="" ).mx-auto.mx-lg-0.ms-lg-auto.mb-4.mb-lg-0
+    
+    .bg-full-width.bg-color-5.mb-5
+      .px-4.px-md-5.py-4
+        p.mb-0 La educación infantil debe ofrecer espacios para la búsqueda de significado, permitiendo que los niños desarrollen propósito y conexión con el mundo.
+    
+    #t_1_7.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.7] Dimensión ética
+    
+    p.mb-5 Desarrollar una conciencia ética en la infancia es clave para que los niños comprendan valores como el respeto, la justicia y la solidaridad (Cortina, 2017). Piaget (1932) señala que los niños pasan de una moral heterónoma (obediencia a la autoridad) a una moral autónoma (comprensión crítica de las normas). Estrategias para fortalecer la ética en el aula:
+
+    .bg-full-width.bg-fondo-3
+      .px-4.px-md-5.pb-md-3
+        .row.justify-content-center.mb-5
+          .col-lg-8.mb-3.mb-lg-0
+            SlyderF(columnas="col-12 col-lg-6")
+              .bg-color-white.p-4.h-100.shadow-app
+                img(src='@/assets/curso/tema1/17.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                h4.text-center.mb-4 Ejemplo de los adultos
+                p.text-center Los niños aprenden observando el comportamiento de sus referentes.
+              .bg-color-white.p-4.h-100.shadow-app
+                img(src='@/assets/curso/tema1/18.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                h4.text-center.mb-4 Diálogo y resolución de conflictos
+                p.text-center Espacios para reflexionar sobre dilemas morales.
+              .bg-color-white.p-4.h-100.shadow-app
+                img(src='@/assets/curso/tema1/19.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                h4.text-center.mb-4 Cuentos con enseñanzas morales
+                p.text-center Ayudan a comprender las consecuencias de las acciones.
+              .bg-color-white.p-4.h-100.shadow-app
+                img(src='@/assets/curso/tema1/20.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                h4.text-center.mb-4 Trabajo en equipo
+                p.text-center Promueve la cooperación y la justicia.
+              .bg-color-white.p-4.h-100.shadow-app
+                img(src='@/assets/curso/tema1/21.svg' alt='AvatarTop' , style="max-width: 90px").mx-auto.mb-3
+                h4.text-center.mb-4 Autonomía y responsabilidad
+                p.text-center Permitir que los niños tomen decisiones y asuman sus consecuencias.
+          .col-lg-4
+            figure
+              img(src='@/assets/curso/tema1/22.png', alt='') 
+    
+    p La ética no se trata solo de reglas, sino de formar ciudadanos críticos y responsables.
+
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
@@ -15,15 +261,16 @@
 
             p.d-flex.my-4
               img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
-
+              a(href="https://doi.org/10.70219/sad-92023-183 " target="_blank" rel="noopener noreferrer") Briceño Portillo, M. (2024). La educación desde las dimensiones del desarrollo humano sustentable. Sustentabilidad al día, (9). 
             p.d-flex.my-4
-              img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-              span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://dimensionesdeldesarrolloinfantil.blogspot.com/p/marco-teorico.html " target="_blank" rel="noopener noreferrer") Blog Dimensiones de Desarrollo Infantil: MARCO TEORICO. (s.f.). Dimensiones de Desarrollo Infantil.  
+
+    
 
             p.d-flex.my-4
               img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-              a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
+              a(href="https://www.youtube.com/watch?v=4vKHA31lZIk " target="_blank" rel="noopener noreferrer") Facultad de Psicología Universidad de la República. (2019, 10 de octubre). Desarrollo, aprendizajes, y educación en la Primera Infancia [Video]. YouTube. 
 
           .col-12.col-md-6.col-lg-3.offset-lg-1
             figure
